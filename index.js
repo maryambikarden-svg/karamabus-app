15da4835752823bab3b5107d60b80a29f4ebaa70
maryambikarden-svg <maryambikarden@gmail.com>
2026-04-11 20:04:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,25 @@ app.get('/', (req, res) => {
 app.get('/api/trajets', (req, res) => {
     const trajets = [
         { ligne: "Ligne 01", itineraire: "Riad Salam - EST" },
-        { ligne: "Ligne 05", itineraire: "Centre Ville - Gare" },
-        { ligne: "Ligne 06", itineraire: "Centre Ville - Université Mghila" },
+        { ligne: "Ligne 02", itineraire: "Beni Mellal - Kssar Daroua" },
+        { ligne: "Ligne 03", itineraire: "Beni Mellal - Igherm Laalam" },
+        { ligne: "Ligne 04", itineraire: "Beni Mellal - Elhlalma" },
+        { ligne: "Ligne 07", itineraire: "Beni Mellal - Ouled Ismail" },
+        { ligne: "Ligne 08", itineraire: "Feryata - Ourbie" },
+        { ligne: "Ligne 09", itineraire: "Beni Mellal - Tanferda" },
+        { ligne: "Ligne 10", itineraire: "Beni Mellal - Takhmisst" },
+        { ligne: "Ligne 12", itineraire: "Beni Mellal - Ouled Nader" },
+        { ligne: "Ligne 13", itineraire: "Beni Mellal - Afourer" },
         { ligne: "Ligne 14", itineraire: "Beni Mellal - Foum Oudi" },
-        { ligne: "Ligne 17", itineraire: "Beni Mellal - Kasba Tadla" }
+        { ligne: "Ligne 15", itineraire: "Beni Mellal - Timoulit" },
+        { ligne: "Ligne 16", itineraire: "Beni Mellal - Igherm Laalam" },
+        { ligne: "Ligne 17", itineraire: "Beni Mellal - Kasba Tadla" },
+        { ligne: "Ligne 18", itineraire: "Beni Mellal - Elksibah" },
+        { ligne: "Ligne 19", itineraire: "Beni Mellal - Elfkih Ben Salah" },
+        { ligne: "Ligne 22", itineraire: "Beni Mellal - Ouled Ayad" },
+        { ligne: "Ligne 24", itineraire: "Ait Tisslit - Mghila" },
+        { ligne: "Ligne 25", itineraire: "Agropole - Mghila" },
+        { ligne: "Ligne 26", itineraire: "Beni Mellal - Oum Elbekht" }
     ];
     res.json(trajets);
 });

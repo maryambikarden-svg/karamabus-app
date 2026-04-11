e2f5ed103cfe2df5630dcb5cf8ac088914c18dfc
maryambikarden-svg <maryambikarden@gmail.com>
2026-04-11 16:09:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Permet au serveur de lire les fichiers (HTML, JSON, etc.) à la racine
 app.use(express.static(__dirname));
 app.use(express.json());
 
-// Route d'accueil : C'est ici qu'on envoie ton fichier HTML
+// ROUTE 1 : Envoie l'interface visuelle (ton index.html)
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Liste des trajets pour l'application
+// ROUTE 2 : Envoie les données des bus à l'interface
 app.get('/api/trajets', (req, res) => {
     res.json([
-        { ligne: "Ligne 01", itineraire: "Riad Salam - Ecole Superieure de Technologie" },
+        { ligne: "Ligne 01", itineraire: "Riad Salam - EST" },
         { ligne: "Ligne 17", itineraire: "Beni Mellal - Kasba Tadla" },
-        { ligne: "Ligne 14", itineraire: "Beni Mellal - Foum Oudi" }
+        { ligne: "Ligne 14", itineraire: "Beni Mellal - Foum Oudi" },
+        { ligne: "Ligne 06", itineraire: "Centre Ville - Université Mghila" }
     ]);
 });
 
